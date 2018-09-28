92ad05c37b58abdbca20508328a15750436a47d9
ZinoOhad <zinoohad@gmail.com>
2018-09-28 10:59:17 +0300

    upda

--- MilitaryHumanResources/MainWindow.xaml.cs
+++ MilitaryHumanResources/MainWindow.xaml.cs
@@ -20,9 +20,60 @@ namespace MilitaryHumanResources
     /// </summary>
     public partial class MainWindow : Window
     {
+        public double WindowWidth { get; set; }
+        public double WindowHeight { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+        private void ExitB_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MinimizeWindowB_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeWindowB_Click(object sender, RoutedEventArgs e)
+        {
+            var b = (Button)sender;
+            switch (WindowState)
+            {
+                case WindowState.Maximized:
+                    WindowState = WindowState.Normal;
+                    this.Height = WindowHeight;
+                    this.Width = WindowWidth;
+                    b.Content = "⬜";
+                    break;
+                case WindowState.Normal:
+                    WindowState = WindowState.Maximized;
+                    b.Content = "❐";
+
+                    break;
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            WindowHeight = this.Height;
+            WindowWidth = this.Width;
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            WindowHeight = this.Height;
+            WindowWidth = this.Width;
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
         }
     }
 }
